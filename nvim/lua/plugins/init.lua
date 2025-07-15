9c889f39984e5a806ab4092d7ed5a897699fcbf3
Chirag Sharma <chirag.sharma342@gmail.com>
2025-07-15 20:59:29 +0530

    modify nvim/lua/plugins/init.lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -17,6 +17,15 @@ return {
   'mrcjkb/rustaceanvim',
   version = '^6', -- Recommended
   lazy = false, -- This plugin is already lazy
+  ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+
+  {
+    "williamboman/mason.nvim"
   },
 
   {
